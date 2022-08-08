eddcdebe37ba9b2c54568415c85e392af9615843
Rohan Dhimal <drowhann@gmail.com>
2022-08-08 21:00:17 +0545

    nav bar completed

---
+++ components/header.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title><?php echo $title ?></title>
+    <script src="https://kit.fontawesome.com/a076d05399.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="styles/styles.css"/>
+  </head>
+  <body>
+    <nav>
+     <h1 class="nav__title">
+      The<span>Blog</span>
+     </h1>
+     <ul class="nav__items">
+      <li class="nav__item"><a href="#">Home</a></li>
+      <li class="nav__item"><a href="#">Trending</a></li>
+      <li class="nav__item"><a href="#">Categories<i class="fas fa-angle-down"></i></a></li>
+      <li class="nav__item"><a href="#">New</a></li>
+      <li class="nav__item"><a href="#">About</a></li>
+      <li class="nav__item"><a href="#">Contact</a></li>
+     </ul>
+    </nav>
--- index.php
+++ index.php
@@ -1,67 +1,40 @@
 
 <?php 
     // Connect to the database
-    include('dbconnection.php');
+    include('db/dbconnection.php');
 
     // Include header file
     $title="Home Page";
-    include('header.php');
+    include('components/header.php');
 ?>
 
 
-<!-- Main -->
-<div class="main">
+   
+<section class="all__blogs">
+    <?php 
+        $sql="SELECT *,category.categoryName from posts 
+        join category on posts.categoryId=category.categoryId";
+        $result=$conn->query($sql);
 
-    <!-- Popular posts -->
-    <div class="popular">
-        <h1>
-            Popular Blogs
-        </h1>
-        <div class="popular__blogs">
-            <?php 
-                $sql="SELECT * from posts order by views desc limit 6";
-                $result=$conn->query($sql);
-
-                if($result->num_rows>0){
-                    while($row=$result->fetch_assoc()){
-                    ?>
-
-                    <div class="blogCard">
-                        <?php echo '<img class="blogCard__img" alt="Banner Image" src="'.$row['bannerImage'].'"/>'; ?>
-                       <div class="blogCard__content">
-                            <?php echo '<span class="blogCard__contentCategory">'.$row['category'].'</span>'; ?>
-                            <?php echo'<p class="blogCard__contentTitle">'.$row['title'].'</p>';?>
-                            <?php echo'<p class="blogCard__contentSummary">'.$row['summary'].'</p>';?>
-                            <div class="blogCard__contentBottom">
-                                <?php 
-                                    $sql='SELECT fname,lname,avatar from users where userId='.$row['userId'];
-                                    $result1=$conn->query($sql);
-                                    $row1=$result1->fetch_assoc();
-
-                                    echo ' <img src="'.$row1['avatar'].'" alt="Avatar">';
-                                ?>
-                               
-                                <div class="blogCard__contentBottomRight">
-                                <?php echo'<p>'.$row1['fname'].' '.$row1['lname'].'</p>';?>
-                                <?php echo'<span class="blogCard__contentBottomTime">'.$row['datePublished'].'</span>';?>
-                                </div>
-                               
-                            </div>
-                       </div>
-                    </div>
-
-                    <?php
-                    }
-                }else{
-                    echo "No blogs to show.";
-                }
+        if($result->num_rows>0){
+            while($row=$result->fetch_assoc()){
             ?>
-        </div>
-    </div>
-    <!-- End popular -->
 
-</div>
-<!-- End main -->
+            <div class="blogCard">
+            <div class="blogCard__banner">
+                <img src=<?php echo $row['bannerImage']?> alt="">
+            </div>
+            </div>
+
+            <?php
+            }
+        }else{
+            echo "No blogs to show.";
+        }
+    ?>
+</section>
+
+
 
 <!-- Include footer -->
-<?php include('footer.php') ?>
+<?php include('components/footer.php') ?>
